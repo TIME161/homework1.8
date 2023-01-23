eba88d5c48cb06b0d073f336afe695746bfc435a
TIME <opout777@yandex.ru>
2023-01-23 20:07:13 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        int[] one = new int[3];
+            one[0] = 1;
+            one[1] = 2;
+            one[2] = 3;
+        double[] two = {1.57, 7.654, 9.986};
+        double[] three = {4.26, 6, 15, 16.38, 28};
     }
 }
