418cc3745abc72e190a906f6d33b42532b4e981c
TIME <opout777@yandex.ru>
2023-01-23 20:55:23 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
-
     public static void task1() {
         System.out.println("Задача 1");
         int[] one = new int[3];
@@ -47,5 +47,38 @@ public class Main {
                 System.out.print(three[i]);
                 }
             }
+        System.out.println();
         }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        double[] two = {1.57, 7.654, 9.986};
+        double[] three = {4.26, 6, 15, 16.38, 28};
+        for (int i = one.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(one[i] + ",");
+            } else {
+                System.out.print(one[i]);
+            }
+        }
+        System.out.println();
+        for (int i = two.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(two[i] + ",");
+            } else {
+                System.out.print(two[i]);
+            }
+        }
+        System.out.println();
+        for (int i = three.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(three[i] + ",");
+            } else {
+                System.out.print(three[i]);
+            }
+        }
+    }
     }
